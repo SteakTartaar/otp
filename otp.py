3718149c799bd85f28ce70bf9f38827942a139cf
steaktartaar <steaktartaar@gmail.com>
2012-12-30 17:24:38 +0100

    Added randomization module

--- otp.py
+++ otp.py
@@ -1,44 +1,96 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import sys
+import argparse
+import os
+import string
+import random
+
+open_files = []
+
+def close_all():
+	for open_file in open_files:
+		open_file.close()
+	alert("Closed all open files")
 
 def err(msg):
-	sys.stderr.write("Unrecoverable error: " + str(msg) + "\n")
+	sys.stderr.write("[X] Unrecoverable error: " + str(msg) + "\n")
 	sys.exit()
 
-class file:
-	filename = ""
-	handle = ""
-	contents = ""
-	mode = ""
+def alert(msg):
+	print("[!] " + str(msg))
 
-	def __init__(self, filename, mode):
-		self.filename = filename
+def get_args():
+	pass
+
+class _file:
+	fn = None
+	handle = None
+	cont = None
+	mode = None
+
+	def __init__(self, fn, mode):
+		self.fn = fn
 		self.mode = mode
 		self.handle = self.open()
+		open_files.append(self)
 
 	def open(self):
 		try:
-			return open(self.filename, self.mode)
+			handle = open(self.fn, self.mode)
+			alert("Opened '" + self.fn + "'")
+			return handle
 		except Exception as ex:
-			err("Unable to open file " + self.filename + " die to error: " + str(ex))
+			err("Unable to open file '" + self.fn + "' due to error: " + str(ex))
 
 	def read(self):
 		try:
-			self.contents = self.handle.read()
+			cont = self.handle.read()
+			alert("Read '" + self.fn + "'")
+			return cont
 		except Exception as ex:
-			err("Unable to read file " + self.filename + " due to error: " + str(ex))
+			err("Unable to read file '" + self.fn + "' due to error: " + str(ex))
+
+	def write(self, data):
+		try:
+			self.handle.write(data)
+			alert("Written to '" + self.fn + "'")
+		except Exception as ex:
+			err("Unable to write to file '" + self.fn + "' due to error: " + str(ex))
 
 	def close(self):
 		try:
 			self.handle.close()
+			open_files.remove(self)
+			alert("Closed '" + self.fn + "'")
 		except Exception as ex:
-			err("Unable to close file " + self.filename + " due to error: " + str(ex))
+			alert("Unable to close file '" + self.fn + "' due to error: " + str(ex))
+
+class rand:
+	src = None
+	def __init__(self):
+		if os.path.exists('/dev/urandom'):
+			self.src = open('/dev/urandom', 'r')
+		elif os.path.exists('/dev/random'):
+			self.src = open('/dev/random', 'r')
+
+	def get_rand(self, size):
+		if self.src == None:
+			return self.get_py_rand(size)
+		else:
+			return self.get_sys_rand(size)
+
+	def get_py_rand(self, size): 
+		return ''.join(random.choice(string.letters + string.digits) for x in range(size))
+
+	def get_sys_rand(self, size):
+		return self.src.read(size)
 
 class crypt:
-	_in = ""
-	_out = ""
-	key = ""
+	_in = None
+	_out = None
+	key = None
+	src = None
 
 	def __init__(self, _in, _out, key):
 		self._in = _in
@@ -50,3 +102,19 @@ class crypt:
 
 	def decrypt(self):
 		pass
+
+	def gen_key(self):
+		pass
+
+def test():
+	random = rand()
+	outfile = _file("out", "w")
+	bits = rand().get_rand(5)
+	outfile.write(bits)
+	close_all()
+
+
+test()
+
+
+
